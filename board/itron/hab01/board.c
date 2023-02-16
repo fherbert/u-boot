f379de1fd6273244785575633dbd691a208b9d90
fherbert <francois@herbert.org.nz>
2023-02-17 09:25:07 +1300

    Add ledblink command to itron

--- board/itron/hab01/board.c
+++ board/itron/hab01/board.c
@@ -1,2 +1,67 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <common.h>
+#include <command.h>
+#include <led.h>
+#include <cyclic.h>
+#include <dm.h>
+
+#if !defined(CONFIG_SPL_BUILD)
+
+struct udevice *dev;
+volatile int ledenable = 0;
+
+
+static void ledblink(void *ctx)
+{
+	if (ledenable) {
+		// Ignore return code, we don't care
+		(void) led_set_state(dev, LEDST_TOGGLE);
+	}
+}
+
+int board_late_init(void)
+{
+	struct cyclic_info *cyclic;
+	int ret;
+
+	ret = led_get_by_label("itron:orange:wifi", &dev);
+	if (ret)
+		printf("LED 'itron:orange:wifi' not found (err=%d)\n", ret);
+
+	/* Register ledblink function to run every 100ms */
+	cyclic = cyclic_register(ledblink, 100 * 1000, "led_blink", NULL);
+	if (!cyclic)
+		printf("Registering of ledblink cyclic function failed\n");
+	return 0;
+}
+
+static int cmd_ledblink(struct cmd_tbl *cmdtp, int flag, int argc,
+			char *const argv[])
+{
+	int ret;
+	if (argc != 2)
+		return CMD_RET_USAGE;
+	if (strncmp(argv[1], "start", 5) == 0) {
+		ledenable = 1;
+		printf("## Starting Led Blink\n");
+		return 0;
+	}
+	if (strncmp(argv[1], "stop", 4) == 0) {
+		ledenable = 0;
+		// Make sure LED is off
+		ret = led_set_state(dev, LEDST_OFF);
+	if (ret < 0) {
+		printf("LED operation failed (err=%d)\n", ret);
+	}
+		printf("## Stopping Led Blink\n");
+		return 0;
+	}
+	return CMD_RET_USAGE;
+}
+
+U_BOOT_CMD(
+	ledblink, 2, 0, cmd_ledblink,
+	"Start/Stop wifi led blinking",
+	"start|stop \t Blink led"
+);
+# endif
--- include/configs/itron-hab01.h
+++ include/configs/itron-hab01.h
@@ -30,11 +30,11 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
     "bootmenu_default=0\0" \
     "bootmenu_0=Run from flash=bootm 0xbc050000 \0" \
-    "bootmenu_1=Flash u-boot from tftp=askenv serverip TFTP Server IP: && askenv ipaddr IP Address: && askenv file_name File name: && tftpboot 0x82000000 ${file_name} && run writeflash\0" \
-    "bootmenu_2=Flash u-boot from USB=setenv bootfile u-boot-with-spl.bin && usb start && load usb 0 && run writeflash\0" \
-    "bootmenu_3=Flash OS from USB=setenv bootfile itronhab01.img && setenv flashoffset 0x50000 && usb start && load usb 0 && run writeflash\0" \
-    "writeflash=sf probe && sf update ${fileaddr} ${flashoffset} 0x${filesize} && reset\0" \
-    "tf=setenv serverip 10.0.0.5 && setenv ipaddr 10.0.0.76 && tftpboot 0x82000000 u-boot-with-spl.bin && run writeflash\0" \
+    "bootmenu_1=Flash u-boot from tftp=askenv serverip TFTP Server IP: && askenv ipaddr IP Address: && askenv file_name File name: && ledblink start && tftpboot 0x82000000 ${file_name} && run writeflash\0" \
+    "bootmenu_2=Flash u-boot from USB=setenv bootfile u-boot-with-spl.bin && ledblink start && usb start && load usb 0 && run writeflash\0" \
+    "bootmenu_3=Flash OS from USB=setenv bootfile openwrt.bin && setenv flashoffset 0x50000 && ledblink start && usb start && load usb 0 && run writeflash\0" \
+    "writeflash=sf probe && sf update ${fileaddr} ${flashoffset} 0x${filesize} && ledblink stop && reset\0" \
+    "tf=setenv serverip 10.0.0.5 && setenv ipaddr 10.0.0.76 && ledblink start && tftpboot 0x82000000 u-boot-with-spl.bin && run writeflash\0" \
     "flashoffset=0\0" \
     "preboot=if button reset; then " \
       "led itron:orange:wifi on;" \
@@ -49,4 +49,3 @@
     "fi;\0" \
 
 #endif /* __CONFIG_ITRON_HAB01_H */
-
